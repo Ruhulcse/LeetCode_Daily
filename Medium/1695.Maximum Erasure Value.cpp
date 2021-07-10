6d6d7f853be7eab95036d02b3d0840786368d427
Ruhulcse <Ruhul.cse7862@gmail.com>
2021-07-11 02:31:00 +0600

    Solved by Hashset+sliding window

---
+++ Medium/1695.Maximum Erasure Value.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maximumUniqueSubarray(vector<int>& nums) {
+        unordered_set<int>value;
+        int left=0,right=0,ans=0;
+        int mx=0;
+        while(right<nums.size()){
+            if(value.find(nums[right])==value.end()){
+                ans+=nums[right];
+                value.insert(nums[right++]);
+                mx=max(mx,ans);
+            }
+            else{
+                ans-=nums[left];
+                value.erase(nums[left++]);
+            }
+        }
+        return mx;
+    }
+};
