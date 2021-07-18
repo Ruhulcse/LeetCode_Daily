f3d90217b70b5d9255ef6724cb7e3e623dc07244
Ruhul amin <ruhul.cse7862@gmail.com>
2021-07-18 23:08:18 +0600

    solved by sliding window

---
+++ Medium/LC-1343.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numOfSubarrays(vector<int>& arr, int k, int threshold) {
+       ios_base::sync_with_stdio(false);
+       cin.tie(NULL);
+       cout.tie(NULL);
+       threshold*=k;
+       int result = accumulate(arr.begin(),arr.begin()+k,0),
+        count = (result>=threshold);
+       for(int i=k;i<arr.size();i++){
+           result+=(arr[i]-arr[i-k]);
+           count+= (result>=threshold);
+       }
+      return count;
+    }
+};
