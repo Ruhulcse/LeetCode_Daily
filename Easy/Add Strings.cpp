020f0881421775f08fccf3e6ec631a6bfb8c40ff
Ruhul amin <ruhul.cse7862@gmail.com>
2021-08-09 22:29:39 +0600

    Brute force solution

---
+++ Easy/Add Strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int i = num1.size()-1;
+        int j = num2.size()-1;
+        int carry = 0;
+        string result;
+        while(i>-1||j>-1){
+            int n = (i>=0?num1[i]-'0':0);
+            int m = (j>=0?num2[j]-'0':0);
+            int sum = n+m+carry;
+            carry = sum/10;
+            result+='0' + (sum % 10);
+            i--;
+            j--;
+        }
+        reverse(result.begin(),result.end());
+        return result;
+    }
+};
