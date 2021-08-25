84b2e62d27fc3c2794787992a75bf9def4ed86e4
Ruhul amin <ruhul.cse7862@gmail.com>
2021-08-25 10:43:55 -0700

    solved by priority queue

---
+++ Medium/Sort character by frequency.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int>mp;
+        unordered_map<char,int>::iterator it;
+        for(auto c: s){
+            mp[c]++;
+        }
+        priority_queue<pair<int,char>>pq;
+        for(it=mp.begin();it!=mp.end();it++){
+            pq.push(make_pair(it->second,it->first));
+        }
+        string result="";
+        while(!pq.empty()){
+            for(int i=0;i<pq.top().first;i++){
+                result+=pq.top().second;
+            }
+            pq.pop();
+        }
+        return result;
+    }
+};
